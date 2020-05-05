908cd0aec5f78eab313c7286a50c01cff34e4aaf
szkmiyabi <led.centipede@gmail.com>
2020-05-06 03:59:36 +0900

    ひな形処理を消すとアプリが停止しないのでデバッグ出力して終了するようにした。

--- m2browser/main.cpp
+++ m2browser/main.cpp
@@ -1,13 +1,16 @@
 ﻿#include "browser.h"
 
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
     /*
+    QApplication a(argc, argv);
     browser w;
     w.show();
     return a.exec();
     */
+    qDebug() <<"app run";
+    return 0;
 }
