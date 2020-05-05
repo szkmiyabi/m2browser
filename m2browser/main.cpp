0aae1c1478173daf5d9f3f075e2b5ef14b7eb4df
szkmiyabi <led.centipede@gmail.com>
2020-05-06 03:51:36 +0900

    ひな形リセット、プロジェクト設定変更

--- m2browser/main.cpp
+++ m2browser/main.cpp
@@ -1,11 +1,13 @@
-#include "browser.h"
+﻿#include "browser.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    /*
     browser w;
     w.show();
     return a.exec();
+    */
 }
