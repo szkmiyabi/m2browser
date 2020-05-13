489c74e62ddb9f2c47a77f155a1f8030a8fc5157
szkmiyabi <led.centipede@gmail.com>
2020-05-14 03:38:44 +0900

    WebPageクラスを実装完了

--- m2browser/webpage.cpp
+++ m2browser/webpage.cpp
@@ -75,3 +75,130 @@ bool WebPage::certificateError(const QWebEngineCertificateError &error)
 #endif
 
 }
+
+
+void WebPage::handleAuthenticationRequired(const QUrl &requestUrl, QAuthenticator *auth)
+{
+
+    QWidget *mainWindow = view()->window();
+    QDialog dialog(mainWindow);
+    dialog.setModal(true);
+    dialog.setWindowFlags(dialog.windowFlags() & ~Qt::WindowContextHelpButtonHint);
+
+    Ui::PasswordDialog passwordDialog;
+    passwordDialog.setupUi(&dialog);
+
+    passwordDialog.m_iconLabel->setText(QString());
+    QIcon icon(mainWindow->style()->standardIcon(QStyle::SP_MessageBoxQuestion, 0, mainWindow));
+    passwordDialog.m_iconLabel->setPixmap(icon.pixmap(32, 32));
+
+    QString introMessage(tr("Enter username and password for \"%1\" at %2")
+                         .arg(auth->realm()).arg(requestUrl.toString().toHtmlEscaped()));
+    passwordDialog.m_infoLabel->setText(introMessage);
+    passwordDialog.m_infoLabel->setWordWrap(true);
+
+    if(dialog.exec() == QDialog::Accepted) {
+        auth->setUser(passwordDialog.m_userNameLineEdit->text());
+        auth->setPassword(passwordDialog.m_passwordLineEdit->text());
+    } else {
+        *auth = QAuthenticator();
+    }
+
+}
+
+
+
+inline QString questionForFeature(QWebEnginePage::Feature feature)
+{
+
+    switch (feature) {
+    case QWebEnginePage::Geolocation:
+        return WebPage::tr("Allow %1 to access your location information?");
+    case QWebEnginePage::MediaAudioCapture:
+        return WebPage::tr("Allow %1 to access your microphone?");
+    case QWebEnginePage::MediaVideoCapture:
+        return WebPage::tr("Allow %1 to access your webcam?");
+    case QWebEnginePage::MediaAudioVideoCapture:
+        return WebPage::tr("Allow %1 to access yor microphone and webcam?");
+    case QWebEnginePage::MouseLock:
+        return WebPage::tr("Allow %1 to lock your mouse cursor?");
+    case QWebEnginePage::DesktopVideoCapture:
+        return WebPage::tr("Allow %1 to capture video of your desktop?");
+    case QWebEnginePage::DesktopAudioVideoCapture:
+        return WebPage::tr("Allow %1 to capture audio and video of your desktop?");
+    case QWebEnginePage::Notifications:
+        return WebPage::tr("Allow %1 to show notification on your desktop?");
+    }
+    return QString();
+
+}
+
+void WebPage::handleFeaturePermissionRequested(const QUrl &securityOrigin, Feature feature)
+{
+
+    QString title = tr("Permission Request");
+    QString question = questionForFeature(feature).arg(securityOrigin.host());
+    if(!question.isEmpty() && QMessageBox::question(view()->window(), title, question) == QMessageBox::Yes)
+        setFeaturePermission(securityOrigin, feature, PermissionGrantedByUser);
+    else
+        setFeaturePermission(securityOrigin, feature, PermissionDeniedByUser);
+
+}
+
+
+void WebPage::handleProxyAuthenticationRequired(const QUrl &, QAuthenticator *auth, const QString &proxyHost)
+{
+
+    QWidget *mainWindow = view()->window();
+    QDialog dialog(mainWindow);
+    dialog.setModal(true);
+    dialog.setWindowFlags(dialog.windowFlags() & ~Qt::WindowContextHelpButtonHint);
+
+    Ui::PasswordDialog passwordDialog;
+    passwordDialog.setupUi(&dialog);
+
+    passwordDialog.m_iconLabel->setText(QString());
+    QIcon icon(mainWindow->style()->standardIcon(QStyle::SP_MessageBoxQuestion, 0, mainWindow));
+    passwordDialog.m_iconLabel->setPixmap(icon.pixmap(32, 32));
+
+    QString introMessage = tr("Connect to proxy \"%1\" using:");
+    introMessage.arg(proxyHost.toHtmlEscaped());
+    passwordDialog.m_infoLabel->setWordWrap(true);
+
+    if(dialog.exec() == QDialog::Accepted) {
+        auth->setUser(passwordDialog.m_userNameLineEdit->text());
+        auth->setPassword(passwordDialog.m_passwordLineEdit->text());
+    } else {
+        *auth = QAuthenticator();
+    }
+
+}
+
+
+void WebPage::handleRegisterProtocolHandlerRequested(QWebEngineRegisterProtocolHandlerRequest request)
+{
+
+    auto answer = QMessageBox::question(
+                view()->window(),
+                tr("Permission Request"),
+                tr("Allow %1 to open all %2 links?")
+                .arg(request.origin().host())
+                .arg(request.scheme())
+    );
+
+    if(answer == QMessageBox::Yes)
+        request.accept();
+    else
+        request.reject();
+
+}
+
+
+#if !defined (QT_NO_SSL) || QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+void WebPage::handleSelectClientCertificate(QWebEngineClientCertificateSelection selection)
+{
+
+    selection.select(selection.certificates().at(0));
+
+}
+#endif
