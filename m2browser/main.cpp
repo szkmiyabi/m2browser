bd4d3533f42a95e7d13cc9f2473807c48a8b7d9e
szkmiyabi <led.centipede@gmail.com>
2020-05-19 01:20:19 +0900

    main.cppを実装
    コンパイル成功

--- m2browser/main.cpp
+++ m2browser/main.cpp
@@ -1,16 +1,44 @@
 ﻿#include "browser.h"
-
+#include "browserwindow.h"
+#include "tabwidget.h"
 #include <QApplication>
+#include <QWebEngineProfile>
+#include <QWebEngineSettings>
 #include <QDebug>
 
-int main(int argc, char *argv[])
+QUrl commandLineUrlArgument()
 {
-    /*
-    QApplication a(argc, argv);
-    browser w;
-    w.show();
-    return a.exec();
-    */
-    qDebug() <<"app run";
-    return 0;
+    const QStringList args = QCoreApplication::arguments();
+    for(const QString &arg : args.mid(1)) {
+        if(!arg.startsWith(QLatin1Char('-')))
+            return QUrl::fromUserInput(arg);
+    }
+    return QUrl(QStringLiteral("https://www.qt.io"));
+}
+
+
+int main(int argc, char **argv)
+{
+
+    QCoreApplication::setOrganizationName("QtExamples");
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+    QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(QStringLiteral(":AppLogoColor.png")));
+
+    QWebEngineSettings::defaultSettings()->setAttribute(QWebEngineSettings::PluginsEnabled, true);
+#if QT_VERSION >= QT_VERSION_CHECK(5, 13, 0)
+    QWebEngineSettings::defaultSettings()->setAttribute(QWebEngineSettings::DnsPrefetchEnabled, true);
+    QWebEngineProfile::defaultProfile()->setUseForGlobalCertificateVerification();
+#endif
+
+    QUrl url = commandLineUrlArgument();
+
+    Browser browser;
+    BrowserWindow *window = browser.createWindow();
+    window->tabWidget()->setUrl(url);
+
+    return app.exec();
+
 }
