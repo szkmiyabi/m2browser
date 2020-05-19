7d1e2fb4b89e680f0669ee9fd348d47083bf8e64
szkmiyabi <led.centipede@gmail.com>
2020-05-20 02:15:45 +0900

    main.cppにコメント追記

--- m2browser/main.cpp
+++ m2browser/main.cpp
@@ -6,6 +6,8 @@
 #include <QWebEngineSettings>
 #include <QDebug>
 
+
+//コマンドラインパラメータの取得
 QUrl commandLineUrlArgument()
 {
     const QStringList args = QCoreApplication::arguments();
@@ -17,28 +19,38 @@ QUrl commandLineUrlArgument()
 }
 
 
+//メイン関数
 int main(int argc, char **argv)
 {
 
+    //アプリの設定
     QCoreApplication::setOrganizationName("JCI-TN");
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 
+    //appのインスタンスを生成しアイコン付与
     QApplication app(argc, argv);
     app.setWindowIcon(QIcon(QStringLiteral(":AppLogoColor.png")));
 
+    //QWebEngineの設定
     QWebEngineSettings::defaultSettings()->setAttribute(QWebEngineSettings::PluginsEnabled, true);
 #if QT_VERSION >= QT_VERSION_CHECK(5, 13, 0)
     QWebEngineSettings::defaultSettings()->setAttribute(QWebEngineSettings::DnsPrefetchEnabled, true);
     QWebEngineProfile::defaultProfile()->setUseForGlobalCertificateVerification();
 #endif
 
+    //コマンドラインから（あれば）URLを読み取る
     QUrl url = commandLineUrlArgument();
 
+    //browser（ウィンドウマネージャ的クラス）のインスタンス
     Browser browser;
+    //最低1個のウィンドウは生成しておく
+    //browserインスタンスからBrowserWindowインスタンスのポインタを受け取る
     BrowserWindow *window = browser.createWindow();
+    //windowポインタからTabWidgetクラスのsetUrlを呼び出し
     window->tabWidget()->setUrl(url);
 
+    //イベントループ開始（要するにアプリの実行）
     return app.exec();
 
 }
