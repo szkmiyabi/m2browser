b00a1c1aeda277bbb9dc6654662c7a8e98c1be33
szkmiyabi <led.centipede@gmail.com>
2020-05-21 15:53:26 +0900

    WebViewクラスにコメント追記

--- m2browser/webview.cpp
+++ m2browser/webview.cpp
@@ -16,21 +16,29 @@ WebView::WebView(QWidget *parent) :
     m_loadProgress(100)
 {
 
+    //Webページロード開始時のファビコン設定の紐付け
     connect(this, &QWebEngineView::loadStarted, [this]() {
         m_loadProgress = 0;
         emit favIconChanged(favIcon());
     });
+
+    //ロード進捗に応じた紐付け
     connect(this, &QWebEngineView::loadProgress, [this](int progress) {
         m_loadProgress = progress;
     });
+
+    //Webページロード完了時のファビコン設定の紐付け
     connect(this, &QWebEngineView::loadFinished, [this](bool success) {
         m_loadProgress = success ? 100 : -1;
         emit favIconChanged(favIcon());
     });
+
+    //Webページのファビコン変更時（URL遷移など）のファビコン設定の紐付け
     connect(this, &QWebEngineView::iconChanged, [this](const QIcon &) {
         emit favIconChanged(favIcon());
     });
 
+    //Webページロードが停止された時の紐付け
     connect(this, &QWebEngineView::renderProcessTerminated,
         [this](QWebEnginePage::RenderProcessTerminationStatus termStatus, int statusCode) {
 
@@ -57,6 +65,7 @@ WebView::WebView(QWidget *parent) :
                    "Do you want to reload the page ?").arg(statusCode)
             );
             if(btn == QMessageBox::Yes)
+                //非同期でWebページをリロードする
                 QTimer::singleShot(0, [this] { reload(); });
         }
     );
@@ -64,6 +73,7 @@ WebView::WebView(QWidget *parent) :
 }
 
 
+//Webページのセットアップ
 void WebView::setPage(WebPage *page)
 {
     createWebActionTrigger(page, QWebEnginePage::Forward);
@@ -74,12 +84,14 @@ void WebView::setPage(WebPage *page)
 }
 
 
+//Webページロード進捗状況を返す
 int WebView::loadProgress() const
 {
     return m_loadProgress;
 }
 
 
+//Webページの基本動作の有効無効化のシグナルを生成する
 void WebView::createWebActionTrigger(QWebEnginePage *page, QWebEnginePage::WebAction webAction)
 {
     QAction *action = page->action(webAction);
@@ -89,12 +101,14 @@ void WebView::createWebActionTrigger(QWebEnginePage *page, QWebEnginePage::WebAc
 }
 
 
+//Webページの基本動作の有効無効の判定
 bool WebView::isWebActionEnabled(QWebEnginePage::WebAction webAction) const
 {
     return page()->action(webAction)->isEnabled();
 }
 
 
+//デフォルトのファビコンを返す
 QIcon WebView::favIcon() const
 {
     QIcon favIcon = icon();
@@ -114,28 +128,34 @@ QIcon WebView::favIcon() const
 }
 
 
+//状況に応じた新しいウィンドウを生成しそのポインタを返す
 QWebEngineView *WebView::createWindow(QWebEnginePage::WebWindowType type)
 {
 
+    //このWebViewクラスが所属するウィンドウを取得
     BrowserWindow *mainWindow = qobject_cast<BrowserWindow*>(window());
     if(!mainWindow)
         return nullptr;
 
     switch (type) {
 
+    //新しいタブで開く
     case QWebEnginePage::WebBrowserTab: {
         return mainWindow->tabWidget()->createTab();
     }
+    //新しいバックグラウンドタブで開く
     case QWebEnginePage::WebBrowserBackgroundTab: {
         return mainWindow->tabWidget()->createBackgroundTab();
     }
+    //カレントタブで開く
     case QWebEnginePage::WebBrowserWindow: {
         return mainWindow->browser()->createWindow()->currentTab();
     }
+    //新しいポップアップウィンドウで開く
     case QWebEnginePage::WebDialog: {
         WebPopupWindow *popup = new WebPopupWindow(page()->profile());
 
-        //developer tool event
+        //DevTools要求時の紐付け
         connect(popup->view(), &WebView::devToolsRequested, this, &WebView::devToolsRequested);
         return popup->view();
     }
@@ -146,6 +166,7 @@ QWebEngineView *WebView::createWindow(QWebEnginePage::WebWindowType type)
 }
 
 
+//Webページのコンテキストメニューのセットアップ
 void WebView::contextMenuEvent(QContextMenuEvent *event)
 {
 
--- m2browser/webview.h
+++ m2browser/webview.h
@@ -4,30 +4,47 @@
 #include <QIcon>
 #include <QWebEngineView>
 
-class WebPage;
+class WebPage;          //WebPageクラスの参照
 
 class WebView : public QWebEngineView
 {
     Q_OBJECT
 
 public:
+
+    //コンストラクタ
     WebView(QWidget *parent = nullptr);
+
+    //Webページのセットアップ
     void setPage(WebPage *page);
 
+    //Webページロード進捗状況を返す
     int loadProgress() const;
+
+    //Webページの基本動作の有効無効の判定
     bool isWebActionEnabled(QWebEnginePage::WebAction webAction) const;
+
+    //デフォルトのファビコンを返す
     QIcon favIcon() const;
 
 protected:
+
+    //Webページのコンテキストメニューのセットアップ
     void contextMenuEvent(QContextMenuEvent *event) override;
+
+    //状況に応じた新しいウィンドウを生成しそのポインタを返す
     QWebEngineView *createWindow(QWebEnginePage::WebWindowType type) override;
 
 signals:
+
+    //WebViewクラス上で発生しうるイベントのシグナル群
     void webActionEnabledChanged(QWebEnginePage::WebAction webAction, bool enabled);
     void favIconChanged(const QIcon &icon);
     void devToolsRequested(QWebEnginePage *source);
 
 private:
+
+    //Webページの基本動作の有効無効化のシグナルを生成する
     void createWebActionTrigger(QWebEnginePage *page, QWebEnginePage::WebAction);
 
 private:
